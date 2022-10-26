71f17dc00060463ed03de34585af64ff15bf9797
akash-k7 <92331024+akash-k7@users.noreply.github.com>
2022-10-26 22:49:33 +0530

    imageSlider.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        navText: ["<img src='stock/previous.png'>","<img src='stock/next.png'>"],
+        items:1,
+        smartSpeed: 1000
+    });
+});
