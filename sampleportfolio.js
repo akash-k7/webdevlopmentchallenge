0eb34d01f86db518370a87f8212326de158d5c8f
AKASH K <92331024+akash-k7@users.noreply.github.com>
2022-11-14 17:01:26 +0530

    Create sampleportfolio.js

---
+++ sampleportfolio.js
@@ -0,0 +1,18 @@
+$(".html").animate({
+    width: "90%"
+}, 2500);
+$(".css").animate({
+    width: "85%"
+}, 2500);
+$(".bs").animate({
+    width: "85%"
+}, 2500);
+$(".js").animate({
+    width: "70%"
+}, 2500);
+$(".php").animate({
+    width: "75%"
+}, 2500);
+$(".wp").animate({
+    width: "80%"
+}, 2500);
